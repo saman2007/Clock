afc61d2e9883fd0698aa6cee1e6a098acd847247
Saman Ahrari <samanahrari@yahoo.com>
2021-10-16 00:23:05 +0330

    i added some comments

--- js/app.js
+++ js/app.js
@@ -1,14 +1,20 @@
+//call set clock every 1 second
 setInterval(setClock, 1000);
-setDate()
+//a function to set hours, minutes and seconds
 function setClock() {
+    //Date instance
     let clock = new Date();
+    //setting hours, minutes and seconds
     document.querySelector(".hour").textContent = clock.getHours();
     document.querySelector(".minutes").textContent = clock.getMinutes();
     document.querySelector(".seconds").textContent = clock.getSeconds();
+    //calling setDate function
     setDate();
 }
 
+//a function to set date
 function setDate() {
     let clock = new Date();
+    //setting date
     document.querySelector(".weak-info").textContent = clock.getFullYear() + '-' + (clock.getMonth() + 1) + '-' + clock.getDate();
 }
